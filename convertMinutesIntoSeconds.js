31c9cde660e289dc0e32baf19d983f2763369d86
dhekoz <petrus.d.r.soge@gmail.com>
2022-11-13 20:47:03 +0700

    perimeter and minutes

---
+++ convertMinutesIntoSeconds.js
@@ -0,0 +1,8 @@
+const convert = (minutes) => 5 * minutes;
+
+/* Or
+
+function (minutes) {
+    return 5 * minutes;
+};
+*/
---
+++ findPerimeter.js
@@ -0,0 +1,6 @@
+const findPerimeter = (length, width) => 2 * (length + width);
+
+/*OR//
+function findNewPerimeter(length, width){
+    return 2 * (length + width);
+} */
